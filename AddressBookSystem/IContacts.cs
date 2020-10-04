8273742a1fb093be0325ad2c2709e4f2f0baaf4f
piyush12346789 <piyushgupta024@gmail.com>
2020-10-04 19:16:48 +0530

    [Piyush] Add: Creating contacts.

---
+++ AddressBookSystem/IContacts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace AddressBookSystem
+{
+    interface IContacts
+    {
+        public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -6,6 +6,9 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program.");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact("Piyush", "Gupta", "JaiShreeRam", "Ramgarh", "MP", "piyush@gmail.com", 123456, 1234567890);
+            addressBook.ViewContact();
         }
     }
 }
