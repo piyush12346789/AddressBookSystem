6f5372b73a562e22a7041dcd008544d3edd2d13d
piyush12346789 <piyushgupta024@gmail.com>
2020-10-04 19:06:19 +0530

    [Piyush] Add: Welcome message.

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program.");
+        }
+    }
+}
